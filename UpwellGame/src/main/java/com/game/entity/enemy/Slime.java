299dc66e7d82ab1b17f303c35d1d2f85f8b61ff9
Jacob-J-Nealy <jacobnealy01@gmail.com>
2026-06-02 09:17:30 -0400

    adjusted Slime Class constructor to build a new slime based on empty created one

--- UpwellGame/src/main/java/com/game/entity/enemy/Slime.java
+++ UpwellGame/src/main/java/com/game/entity/enemy/Slime.java
@@ -7,10 +7,7 @@ public class Slime extends Entity {
 
 
     // Slime Constructor
-    public Slime(String name, int curentHealthpoints, int maxHealthpoints, int levelAddress, Weapon weapon, int damageReduction) {
-        super("GREEN SLIME", 25, 25, 1, new Acid(8,2), 25);
-    }
-
     public Slime() {
+        super("GREEN SLIME", 25, 25, 1, new Acid(8,2), 25);
     }
 }
