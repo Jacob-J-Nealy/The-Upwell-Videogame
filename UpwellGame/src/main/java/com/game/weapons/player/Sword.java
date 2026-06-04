c5d080e3c9aee69064174f1eb8c919fd69fe1c15
Jacob-J-Nealy <jacobnealy01@gmail.com>
2026-06-04 14:11:32 -0400

    added functionality for maina and heavy attack for sword

--- UpwellGame/src/main/java/com/game/weapons/player/Sword.java
+++ UpwellGame/src/main/java/com/game/weapons/player/Sword.java
@@ -5,7 +5,7 @@ import com.game.weapons.Weapon;
 
 public class Sword extends Weapon {
 
-    // Constructor
+    //  Sword Constructor
     public Sword(String name, int attackPower, int defensePower) {
         super(name, attackPower, defensePower);
     }
@@ -17,17 +17,67 @@ public class Sword extends Weapon {
     }
 
     // Player Action Methods
+    @Override
     public int mainAttack() {
-        return 0;
+        /**
+         * Functionality Explained:
+         *      -> 5% Chance of Missing
+         *      -> 25% Chance of Low Damage
+         *      -> 70% Chance of Average Damage
+         *      (All Damage variates +5 and -5 of base value)
+         */
+
+        double chance = Math.random();
+        if (chance <= 0.05) {
+            return 0; // missed
+        }
+        else if (chance > 0.05 && chance <= 0.30) {
+            return applyVariance((int)(getAttackPower() * 0.7), 5); // low damage
+        }
+        else {
+            return applyVariance(getAttackPower(),5); // average damage
+        }
     }
+    @Override
     public int heavyAttack() {
+        /**
+         * Functionality Explained:
+         *      -> 10% Chance of Missing
+         *      -> 25% Chance of Low Damage
+         *      -> 25% Chance of Average Damage
+         *      -> 35% Chance of High Damage
+         *      -> 5% Insanely High Damage
+         *      (All Damage variates +5 and -5 of base value)
+         */
 
-        return 0;
+        double chance = Math.random();
+
+        if (chance < 0.10) {
+            return 0; // missed
+        } else if (chance < 0.35) {
+            return applyVariance((int)(getAttackPower() * 0.6), 5); // low damage
+        } else if (chance < 0.60) {
+            return applyVariance((getAttackPower()),5); // average damage
+        } else if (chance < 0.95) {
+            return applyVariance((int)(getAttackPower() * 1.4),5); // high damage
+        } else {
+            return applyVariance((int)(getAttackPower() * 2.0),10);
+        }
+
     }
+    @Override
     public int specialAttack() {
+        /**
+         * Functionality Explained:
+         *      -> 50% Chance of Missing
+         *      -> If hits, deals 5 damage and stuns enemy
+         */
+
+        double stunChance = Math.random();
 
         return 0;
     }
+    @Override
     public int defend() {
 
         return 0;
